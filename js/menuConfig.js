0631c599850cac72488c4ef11c8b2eb7ef876825
oyamairuka <eijishimizu9@gmail.com>
2023-09-09 13:44:40 +0900

    大枠を最小限にする

--- js/menuConfig.js
+++ js/menuConfig.js
@@ -1,12 +1,15 @@
 const menuList = [
-    { "name": "ホーム", "path": "home.html" },
-    { "name": "特徴", "path": "feature.html" },
-    { "name": "サービス", "path": "service.html" },
-    { "name": "会社情報", "path": "company.html" },
+    { "name": "ホーム", "path": "index.html" },
+    { "name": "とくちょう", "path": "index.html" },
+    // { "name": "とくちょう", "path": "feature.html" },
+    // { "name": "サービス", "path": "service.html" },
+    // { "name": "会社情報", "path": "company.html" },
 ];
 
 // フッター部分のみに追加するメニュー
 const footerMenu = [
-    { "name": "サイトマップ", "path": "sitemap.html" },
-    { "name": "プライバシーポリシー", "path": "privacy.html" },
+    { "name": "サイトマップ", "path": "index.html" },
+    { "name": "プライバシーポリシー", "path": "index.html" },
+    // { "name": "サイトマップ", "path": "sitemap.html" },
+    // { "name": "プライバシーポリシー", "path": "privacy.html" },
 ];
